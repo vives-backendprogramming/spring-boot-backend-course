43d15c3343a19ef703e8cbbbbe7c8b136f2bd60f
Yves Seurynck <yves.seurynck@vives.be>
2026-09-16 09:24:25 +0200

    lesson-04 intro spring boot bijwerken

---
+++ lesson-04-spring-boot-intro/pizzastore-intro/src/main/java/be/vives/pizzastore/domain/Pizza.java
@@ -0,0 +1,60 @@
+package be.vives.pizzastore.domain;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "pizzas")
+public class Pizza {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal price;
+
+    public Pizza() {
+    }
+
+    public Pizza(String name, BigDecimal price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Pizza{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
